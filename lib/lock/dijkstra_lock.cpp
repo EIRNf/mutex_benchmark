3e4118b7117cf2d18e506b7c9a711bc4ad5bad10
Kush Goel <kush.goel@scriptsense.co.nz>
2025-06-30 11:09:45 -0700

    added fences to dijkstra

--- lib/lock/dijkstra_lock.cpp
+++ lib/lock/dijkstra_lock.cpp
@@ -17,25 +17,32 @@ public:
     void lock(size_t thread_id) override {
         // TODO refactor and remove goto
         unlocking[thread_id] = false;
+        // std::atomic_thread_fence(std::memory_order_seq_cst);
     try_again:
+        c[thread_id] = true;
+        std::atomic_thread_fence(std::memory_order_seq_cst);
         if (k != thread_id) {
-            c[thread_id] = true;
-            if (unlocking[k]) {
-                k = thread_id;
-            }
+        // std::atomic_thread_fence(std::memory_order_seq_cst);
+            while (!unlocking[k]) {}
+            // std::atomic_thread_fence(std::memory_order_seq_cst);
+            k = thread_id;
+            std::atomic_thread_fence(std::memory_order_seq_cst);
+            
             goto try_again;
-        } else {
-            c[thread_id] = false;
-            for (size_t j = 0; j < num_threads; j++) {
-                if (j != thread_id && !c[j]) {
-                    goto try_again;
-                }
+        } 
+        c[thread_id] = false;
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        for (size_t j = 0; j < num_threads; j++) {
+            if (j != thread_id && !c[j]) {
+                goto try_again;
             }
         }
     }
     void unlock(size_t thread_id) override {
         c[thread_id] = true;
+        std::atomic_thread_fence(std::memory_order_seq_cst);
         unlocking[thread_id] = true;
+        std::atomic_thread_fence(std::memory_order_seq_cst);
     }
     void destroy() override {
         free((void*)unlocking);
